9e3db68f92f8967397e8f38e9deb9393ff0a3481
Thanapon Teerakaisri <dev.thanapon@gmail.com>
2023-09-24 19:07:04 +0700

    add fish example config

---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -x PATH $PATH /usr/local/go/bin $HOME/.cargo/bin $HOME/go/bin $HOME/.local/bin
+set -x FLYCTL_INSTALL /home/pui/.fly
+set -x PATH $PATH $FLYCTL_INSTALL/bin
+set -x PATH $PATH $HOME/.local/tmux-workspaces
+
+bind \cf 'tmux-sessionizer'
+
+alias vim="nvim"
+alias lsws="ls ~/.local/tmux-workspaces"
+
+starship init fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
